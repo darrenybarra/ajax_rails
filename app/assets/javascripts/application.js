1af6f5f21f9ac1851b2ba4148565524add47caa2
Darren Ybarra <darrenybarra@gmail.com>
2018-06-10 17:06:33 -0500

    added jquery autocomplete feature in the navbar search form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,15 @@
 //= require jasny-bootstrap.min
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery-ui
 //= require_tree .
+
+$(function() {
+    $('#term').autocomplete({
+        source: '/contacts/autocomplete',
+        minLength: 3,
+        select: function(event, ui) {
+            $('#term').val(ui.item.value);
+        }
+    });
+});
