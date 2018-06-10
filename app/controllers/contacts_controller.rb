cae26561f9ec6b639b0e86fe5902ba752d16903b
Darren Ybarra <darrenybarra@gmail.com>
2018-06-09 23:14:23 -0500

    added validation to contact model

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,14 +2,14 @@ class ContactsController < ApplicationController
   def index
     if params[:group_id] && !params[:group_id].empty?
       # @contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
-      @contacts = Group.find(params[:group_id]).contacts.page(params[:page])
+      @contacts = Group.find(params[:group_id]).contacts.order(created_at: :desc).page(params[:page])
     else
-      @contacts = Contact.page(params[:page])
+      @contacts = Contact.order(created_at: :desc).page(params[:page])
     end
   end
 
   def new
-    
+    @contact = Contact.new.order(created_at: :desc)
   end
 
   def create
