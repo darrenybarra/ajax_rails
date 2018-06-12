ba8600a9e1a28e98d6cb7dca654633840f2152a5
Darren Ybarra <darrenybarra@gmail.com>
2018-06-11 21:17:11 -0500

    added user references to contact and group

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180610233041) do
+ActiveRecord::Schema.define(version: 20180612021512) do
 
   create_table "contacts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -25,11 +25,15 @@ ActiveRecord::Schema.define(version: 20180610233041) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "user_id"
     t.index ["group_id"], name: "index_contacts_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_contacts_on_user_id", using: :btree
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "name"
+    t.string  "name"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_groups_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -51,4 +55,6 @@ ActiveRecord::Schema.define(version: 20180610233041) do
   end
 
   add_foreign_key "contacts", "groups"
+  add_foreign_key "contacts", "users"
+  add_foreign_key "groups", "users"
 end
