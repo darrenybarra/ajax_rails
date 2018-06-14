24d162bceef4e2538a45e698180a4dde8133b4df
Darren Ybarra <darrenybarra@gmail.com>
2018-06-14 01:57:41 -0500

    implemented spin js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,11 +24,13 @@
 //= require jquery-ui/widgets/autocomplete
 //= require jquery-ui/widgets/menu
 //= require toastr
+//= require spin
+//= require jquery.spin
 //= require_tree .
 
 $(document).on('turbolinks:load', function() {
     $('#term').autocomplete({
-        source: '/contacts/autocomplete',
+        source: "/contacts/autocomplete",
         minLength: 3,
         select: function(event, ui) {
             $('#term').val(ui.item.value);
@@ -39,12 +41,62 @@ $(document).on('turbolinks:load', function() {
     $('#form-modal-save-btn').click(function() {
         $('#form-modal-body').find('form').submit();
     });
+
+    $('#ajax-spin').spin(false);
+});
+
+$(document).ajaxStart(function() {
+    $('#ajax-spin').spin({
+        lines: 13 // The number of lines to draw
+            ,
+        length: 28 // The length of each line
+            ,
+        width: 14 // The line thickness
+            ,
+        radius: 42 // The radius of the inner circle
+            ,
+        scale: 1 // Scales overall size of the spinner
+            ,
+        corners: 1 // Corner roundness (0..1)
+            ,
+        color: '#000' // #rgb or #rrggbb or array of colors
+            ,
+        opacity: 0.25 // Opacity of the lines
+            ,
+        rotate: 0 // The rotation offset
+            ,
+        direction: 1 // 1: clockwise, -1: counterclockwise
+            ,
+        speed: 1 // Rounds per second
+            ,
+        trail: 60 // Afterglow percentage
+            ,
+        fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+            ,
+        zIndex: 2e9 // The z-index (defaults to 2000000000)
+            ,
+        className: 'spinner' // The CSS class to assign to the spinner
+            ,
+        top: '50%' // Top position relative to parent
+            ,
+        left: '50%' // Left position relative to parent
+            ,
+        shadow: false // Whether to render a shadow
+            ,
+        hwaccel: false // Whether to use hardware acceleration
+            ,
+        position: 'absolute' // Element positioning
+    });
+});
+
+$(document).ajaxStop(function() {
+    $('#ajax-spin').spin(false);
 });
 
 $(document).on('click', '.pagination a[data-remote=true], a.list-group-item', function() {
     history.pushState({}, '', $(this).attr('href'));
 });
 
-$(window).on('popState', function() {
+$(window).on('popstate', function() {
     $.get(document.location.href);
-})
+});
